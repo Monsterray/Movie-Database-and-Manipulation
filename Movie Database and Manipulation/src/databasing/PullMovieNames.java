c8eb7e349c63a2c184fce2e6ec917e713a1aa29d
Monsterray <inkrpen@gmail.com>
2017-03-09 14:09:25 -0800

    Finished reworking the entire program to use Maps Program now makes two files "Movie Titles Sorted By Title" and "Movie Titles Sorted By Year", the sorted information is sorted by title and year respectivly I'm in the works on figuring out how to get IMDB information

--- Movie Database and Manipulation/src/databasing/PullMovieNames.java
+++ Movie Database and Manipulation/src/databasing/PullMovieNames.java
@@ -22,16 +22,23 @@ import java.util.TreeMap;
  *
  */
 public class PullMovieNames {
+
+	private String fileNameIn;
+	private Writer writer;
+	private static long startTime;
+	private static long endTime;
+	private static long totalTime;
+	private String fileTitle = "Movie Titles";
+	
 	/**
-	 * @param args
+	 * This what runs when the program is started. All this does is start the run() method that actually starts the program up.
+	 * @param args input from when the program was started
 	 */
 	public static void main(String[] args) {
 		System.out.println("Initializing...");
 		PullMovieNames pullMovieNames = new PullMovieNames();
 
-		try {
-			pullMovieNames.run();
-		} catch (IOException e) {e.printStackTrace();}
+		pullMovieNames.run();
 		
 		endTime = System.currentTimeMillis();
 		totalTime = (endTime - startTime);
@@ -39,21 +46,35 @@ public class PullMovieNames {
 		System.out.println("Finished in " + totalTime / 1000 + " miliseconds!");
 	}
 
-	private String fileNameIn;
-	private Writer w;
-	private static long startTime;
-	private static long endTime;
-	private static long totalTime;
-	private String fileTitle = "Movie Titles";
+	/**
+	 * This is the method that does all of the hard work for the entire program.
+	 */
+	public void run() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input file location: ");
+		fileNameIn = in.nextLine();
+
+		System.out.println("Running...");
+		startTime = System.currentTimeMillis();
+		
+		Map<String, Integer> movies = new HashMap<String, Integer>();
+		File currentFolder = new File(fileNameIn);
+
+		fillMapInfo(currentFolder, movies);
+		try {
+			sortMovies(movies);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		in.close();
+	}
 	
 	/**
 	 * Finds movies and adds their name and year released into a file at location
 	 * @param location String, is the path of the parent folder to be searched
 	 */
-	public void outputInfo(String location, Map<String, Integer> map) {	// format is "Name [extra info] year"
-		File currentFolder = new File(location);
-		File[] allFiles = currentFolder.listFiles();
-
+	public void fillMapInfo(File parentFolder, Map<String, Integer> map) {	// format is "Name [extra info] year"
+		File[] allFiles = parentFolder.listFiles();
 		for (int i = 0; i < allFiles.length; i++) { // In the middle of changing this to only grab the info and adding it to a map to be 
 //													   sorted and then I need to add a way to write the data to files
 			String extension = allFiles[i].getName().substring(allFiles[i].getName().lastIndexOf('.'), allFiles[i].getName().length());
@@ -66,40 +87,56 @@ public class PullMovieNames {
 					System.out.println("Exception");
 				}
 			} else if (allFiles[i].isDirectory()) {
-				outputInfo(allFiles[i].getAbsolutePath(), map);
+				fillMapInfo(allFiles[i], map);
 			}
 		}
 	}
 	
 	/**
-	 * Finds movies and adds their name and year released into a map to be sorted and put into a file at location
-	 * @param location String, is the path of the parent folder to be searched
+	 * Sorts the movies in the Map by title and then by year and puts them into two different files
+	 * @param movies Map<String, Integer>, is the Map that is full of all the movies and the years they came out
+	 * @throws IOException 
 	 */
-	public void sortMovies(String location){
-
-		HashMap<String, Integer> movies = new HashMap<String, Integer>();
-		outputInfo(location, movies);
-
+	public void sortMovies(Map<String, Integer> movies) throws IOException{
+		File statText = new File("./" + fileTitle + " Sorted By Title.txt");
+		FileOutputStream is = new FileOutputStream(statText);
+		OutputStreamWriter osw = new OutputStreamWriter(is);
+		writer = new BufferedWriter(osw);
+		
 		Map<String, Integer> sortedByTitleMovies = sortByKey(movies);
 		for (Entry<String, Integer> entry : sortedByTitleMovies.entrySet()){
-		    System.out.println("\t" + entry.getKey() + "/ " + entry.getValue());
+			writer.write(entry.getKey() + " " + entry.getValue() + "\n");
 		}
+		writer.close();
 		
+		writer = new BufferedWriter(
+					new OutputStreamWriter(
+					new FileOutputStream(
+					new File("./" + fileTitle + " Sorted By Year.txt"))));
 		Map<String, Integer> sortedByYearMovies = sortByValue(movies);
 		for (Entry<String, Integer> entry : sortedByYearMovies.entrySet()){
-		    System.out.println("\t\t" + entry.getKey() + "/ " + entry.getValue());
+			writer.write(entry.getValue() + " " + entry.getKey() + "\n");
 		}
 		
-		System.out.println(movies.size() + " movies!");
+		System.out.println(movies.size() + " movies found, sorted, and writen to files!");
+		writer.close();
 	}
-	
-	private Map<String, Integer> sortByKey(Map<String, Integer> unsortMap) {
-		Map<String, Integer> sortedMap = new TreeMap<String, Integer>(unsortMap);
+
+	/**
+	 * Takes in a Map and puts it into a tree map which automatically sorts the map by key
+	 * @param unsortMap the map to be sorted
+	 */
+	private <k, v> Map<k, v> sortByKey(Map<k, v> unsortMap) {
+		Map<k, v> sortedMap = new TreeMap<k, v>(unsortMap);
 
 		
 		return sortedMap;
 	}
 	
+	/**
+	 * Takes in a Map and puts it into a tree map which automatically sorts the map by key
+	 * @param unsortMap the map to be sorted
+	 */
     private Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
 
         // 1. Convert Map to List of Map
@@ -121,27 +158,4 @@ public class PullMovieNames {
         }
         return sortedMap;
     }
-
-	/**
-	 * 
-	 */
-	public void run() throws IOException {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Input file location: ");
-		fileNameIn = in.nextLine();
-
-		System.out.println("Running...");
-		startTime = System.currentTimeMillis();
-
-		File statText = new File("./" + fileTitle + ".txt");
-		FileOutputStream is = new FileOutputStream(statText);
-		OutputStreamWriter osw = new OutputStreamWriter(is);
-		w = new BufferedWriter(osw);
-		
-//		outputInfo(fileNameIn);
-		sortMovies(fileNameIn);
-		
-		w.close();
-		in.close();
-	}
 }
