f257a9756b988e2a653f804ea3f2b398ca77247c
Monsterray <inkrpen@gmail.com>
2017-03-24 19:58:55 -0700

    Changed the names of methods to something that makes more sense

--- Movie Database and Manipulation/src/databasing/Movie.java
+++ Movie Database and Manipulation/src/databasing/Movie.java
@@ -4,6 +4,7 @@
 package databasing;
 
 import java.awt.Image;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -70,37 +71,59 @@ public class Movie {
 	 *	@param title This is the normal name of the movie to get details about
 	 */
 	public Movie(String title){
-		this.movieTitle = title.substring(0, title.length() - 4);
-		String site = "http://www.imdb.com/find?ref_=nv_sr_fn&q=" + title.replace(" ", "+") + "&s=all";	// http://www.imdb.com/find?ref_=nv_sr_fn&q=12+Years+a+Slave+2013&s=all
-
+		this.movieTitle = title; // .substring(0, title.length() - 4)
+		title = title.replace(" ", "+");
+		String site = "http://www.imdb.com/find?ref_=nv_sr_fn&q=" + title + "&s=all";
 		try{
 			UserAgent userAgent = new UserAgent();	//create new userAgent (headless browser).
-			userAgent.visit(site);	//visit a url
-			Element firstResult = userAgent.doc.findFirst("<tr class=\"findResult odd\">"); // Find the first result on the search page
-			String movieAddress = firstResult.findFirst("<a href>").getAt("href");	// Get the first hyperlink that contains the movie we are looking for
-			
+			userAgent.visit(site);	//visit the search result page for the title of the movie
+			Element searchPage = userAgent.doc;
+
+			Elements oddResults = searchPage.findEvery("<tr class=\"findResult odd\">"); // Find all tr's who's class matches "findResult odd"
+			Elements evenResults = searchPage.findEvery("<tr class=\"findResult even\">"); // Find all tr's who's class matches "findResult even"
+			Elements interleaved = interleaveElements(oddResults, evenResults);
+			String movieAddress = null;
+			for(Element e : interleaved){	// Looks for the first result that doesn't include "(TV Series)" or "(TV Episode)"
+				if(!e.innerText().contains("(TV Episode)") || !e.innerText().contains("(TV Series)")){
+					movieAddress = e.findFirst("<a href>").getAt("href");
+					break;
+				}
+			}
 			
 			userAgent.visit(movieAddress);//visit a url
 			Element moviePage = userAgent.doc;
-			this.movieRating = new Double(moviePage.findFirst("<span itemprop=\"ratingValue\">").innerText());	// find the rating of the movie on IMDb
+			this.movieRating = new Double(moviePage.findFirst("<span itemprop=\"ratingValue\">").innerText());	// Find the rating of the movie on IMDb
 			Element outerPublishDate = moviePage.findFirst("<meta itemprop=\"datePublished\">");
 			String publishHTML = outerPublishDate.outerHTML();
 			releaseDate = publishHTML.substring(publishHTML.length() - 12, publishHTML.length() - 2);
-			Element outerContentRaiting = moviePage.findFirst("<meta itemprop=\"contentRating\">");
+			Element outerContentRaiting = moviePage.findFirst("<meta itemprop=\"contentRating\">");	// Find the content raiting of the movie
 			String content = outerContentRaiting.getAt("content");
 			contentRating = content.substring(content.length() - 1, content.length());
-			Element outerDuration = moviePage.findFirst("<time itemprop=\"duration\">");
+			Element outerDuration = moviePage.findFirst("<time itemprop=\"duration\">");	// Find the run time of the movie
 			runTime = outerDuration.innerText().trim();
-			Element outerSummary = moviePage.findFirst("<div class=\"summary_text\">");
+			Element outerSummary = moviePage.findFirst("<div class=\"summary_text\">");	// Find the summary of the movie
 			summary = outerSummary.innerText().trim();
-			Element outerDirector = moviePage.findFirst("<span itemprop=\"director\">");
+			Element outerDirector = moviePage.findFirst("<span itemprop=\"director\">");	// Find the director of the movie
 			director = outerDirector.innerText().trim();
-			Elements ElementalWriters = moviePage.findEvery("<span itemprop=\"creator\" itemtype=\"http://schema.org/Person\">");
+			Elements ElementalWriters = moviePage.findEvery("<span itemprop=\"creator\" itemtype=\"http://schema.org/Person\">");	// Find the writers of the movie
 			writers = new LinkedList<String>();
 			for(Element e : ElementalWriters){
 				writers.add(e.innerText().trim());
 			}
-			Element outerMetascore = moviePage.findFirst("<div class=\"metacriticScore score_favorable titleReviewBarSubItem\">");	// <div class="metacriticScore score_favorable titleReviewBarSubItem">
+			Element outerMetascore = null;
+			try {
+				outerMetascore = moviePage.findFirst("<div class=\"metacriticScore score_favorable titleReviewBarSubItem\">");	// Find the Metacritic score ( a little harder than previously thought)
+			} catch (JauntException je) {
+				try {
+					outerMetascore = moviePage.findFirst("<div class=\"metacriticScore score_mixed titleReviewBarSubItem\">");	// Find the Metacritic score ( a little harder than previously thought)
+				} catch (Exception e1) {
+					try {
+						outerMetascore = moviePage.findFirst("<div class=\"metacriticScore score_unfavorable titleReviewBarSubItem\">");
+					} catch (JauntException je2) {
+							
+					}
+				}
+			}
 			metascore = Integer.parseInt(outerMetascore.innerText().trim());
 			Elements genres = userAgent.doc.findEvery("<span itemprop=\"genre\">");
 			movieGenres = new LinkedList<String>();
@@ -113,11 +136,39 @@ public class Movie {
 	}
 	
 	/**
+	 * <div>Method that takes in two Elements Objects and interleaves the two together starting with the first Elements Object</div>
+	 * <br>
+	 * <div>Example:<br>
+	 * oddResults: [1,3,5]<br>
+	 * evenResults: [2,4,6]<br><br>
+	 * 
+	 * interleavedElements: [1,2,3,4,5,6]</div>
+	 * 
+	 *  @param oddResults The elements that the method will start with
+	 *  @param evenResults The elements that the method will interleave with the odd results
+	 *	@return An Elements Object with the two Elements Objects interleaved with each other
+	 */
+	private Elements interleaveElements(Elements oddResults, Elements evenResults) {	// Possibly don't use iterator?
+		Elements interleavedElements = new Elements();
+		Iterator<Element> oddElementIter = oddResults.iterator();
+		Iterator<Element> evenElementIter = evenResults.iterator();
+		while(oddElementIter.hasNext() || evenElementIter.hasNext()){
+			if(oddElementIter.hasNext()){
+				interleavedElements.addChild(oddElementIter.next());
+			}
+			if(evenElementIter.hasNext()){
+				interleavedElements.addChild(evenElementIter.next());
+			}
+		}
+		return (Elements) interleavedElements;
+	}
+	
+	/**
 	 * Method that gets the title of the movie
 	 * 
 	 *	@return The string of the movie or null
 	 */
-	public String getMovieTitle() {
+	public String getTitle() {
 		return movieTitle;
 	}
 
@@ -135,7 +186,7 @@ public class Movie {
 	 * 
 	 *	@return A List of genres or null
 	 */
-	public List<String> getMovieGenres() {
+	public List<String> getGenres() {
 		return movieGenres;
 	}
 
